cf9fdd2e6ac6cb60c23266a71020bdb3b4dc8080
chalanaKahandawala <chalanak7@gmail.com>
2024-02-15 00:46:37 +0530

    initial commit
    initial commit with basic project structures.

---
+++ src/test/java/com/Accu/nopCommenrce/BuildYourOwnComputer.java
@@ -0,0 +1,29 @@
+package com.Accu.nopCommenrce;
+
+public class BuildYourOwnComputer {
+	public static void main(String[] args) {
+		/*		System.setProperties("webdriver.chrome.driver", "F:/Automation/chromedriver_win32/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+
+        // Navigate to a website
+        driver.get("https://demo.nopcommerce.com/");
+        
+        //Capture the clickable element
+        WebElement lnkComputer = driver.findElement(By.xpath("//a[text()='Build your own computer']"));
+        
+        //Click the link and navigate
+        lnkComputer.click();
+        
+        //Capture header element of the item
+        WebElement hItem = driver.findElement(By.xpath("//h1[text()='Build your own computer']"));
+        
+        //Validate landing the correct page
+        Assert.isTrue(hItem.isDisplayed(), "Item header displays successfully", args);
+        
+        //Capture element processor
+        WebElement ddprocessor = driver.findElement(By.xpath("//select[@id='product_attribute_1']"));
+        Select sProcessor = new Select(ddprocessor);
+        sProcessor.selectByVisibleText("2.2 GHz Intel Pentium Dual-Core E2200");*/
+	}
+
+}
